02057414d753030a19d3ddd504654acdb59a3784
soniyabhoite <soniyabhoite223@gmail.com>
2024-05-23 13:08:11 +0530

    update

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import axios from "axios";
+import {UserContextProvider} from "./UserContext";
+import Routes from "./Routes";
+
+function App() {
+  axios.defaults.baseURL = 'http://localhost:4040';
+  axios.defaults.withCredentials = true;
+  return (
+    <UserContextProvider>
+      <Routes />
+    </UserContextProvider>
+  )
+}
+
+export default App;
